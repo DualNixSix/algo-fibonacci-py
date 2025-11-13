8c15f58c7dd55425d5f67eb0413303be8f5b106c
DualNixSix <H4L0R34CHFTW@HOTMAIL.COM>
2025-11-12 19:40:30 -0700

    Completed Fibonacci Sequence Python assignment

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,10 @@
 def fibonacci(n):
-  pass
+  if n == 0:  # 0 returns 0
+    return 0
+  elif n == 1 or n == 2: # 1 or 2 returns 1 
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2) # use the formula F(n) = F(n-1) + F(n-2)
+pass
 
 
